af7356770fea0093c3f86b0ae31148dc7169ab37
Swapnil Talele <swapniltalele00@gmail.com>
2019-11-08 14:12:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,135 @@
+<?php include('header.php');?>
+
+      <!-- Breadcrumbs -->
+
+      <section>
+
+        <div class="breadcrumbs-custom context-dark bg-overlay-33">
+
+          <div class="container">
+
+            <h2 class="breadcrumbs-custom-title">CCNTS</h2>
+
+            <ul class="breadcrumbs-custom-path">
+
+              <li><a href="index.html">Home</a></li>
+
+              <li class="active"></li>
+
+            </ul>
+
+          </div>
+
+          <div class="box-position" style="background-image: url(images/banner.jpg);"></div>
+
+        </div>
+
+      </section>
+
+      <!-- Section-services-->
+
+      <section class="section section-xl bg-default">
+
+        <div class="container">
+
+          <div class="row row-40">
+
+            <div class="col-md-6 col-lg-4">
+
+              <article class="box-icon-modern ">
+                
+
+                <img src="images/jail.jpg" alt="" />
+
+                <h5 class="box-icon-modern-title"><a href="arresteddisplay.php">Arrested</a></h5>
+
+                <div class="box-icon-modern-decor"></div>
+
+                
+               <a href="arresteddisplay.php" class="knowmorebtn-services">View More</a>
+              </article>
+
+            </div>
+
+            <div class="col-md-6 col-lg-4">
+
+              <article class="box-icon-modern ">
+
+                 <img src="images/e-challan.jpg" alt=""/>
+
+                <h5 class="box-icon-modern-title"><a href="challandisplay.php">E-Challan</a></h5>
+
+                <div class="box-icon-modern-decor"></div>
+
+                <a href="challandisplay.php" class="knowmorebtn-services">View More</a>
+              </article>
+
+            </div>
+
+            <div class="col-md-6 col-lg-4">
+
+              <article class="box-icon-modern">
+
+                <img src="images/misingmobile.jpg" alt="" "/>
+
+                <h5 class="box-icon-modern-title"><a href="missingmobileform.php">Missing Mobile</a></h5>
+
+                <div class="box-icon-modern-decor"></div>
+
+                <a href="missingmobileform.php" class="knowmorebtn-services">View More</a>
+              </article>
+
+            </div>
+
+            <div class="col-md-6 col-lg-4">
+
+              <article class="box-icon-modern">
+
+                 <img src="images/vehical.png" alt="" width="270" height="300"/>
+                <h5 class="box-icon-modern-title"><a href="missingvehicaldisplay.php">Missing Vehical</a></h5>
+
+                <div class="box-icon-modern-decor"></div>
+
+                <a href="missingvehicaldisplay.php" class="knowmorebtn-services">View More</a>
+              </article>
+
+            </div>
+
+            <div class="col-md-6 col-lg-4">
+
+              <article class="box-icon-modern">
+
+                <img src="images/wanted.jpg" alt="" width="270" height="300"/>
+
+                <h5 class="box-icon-modern-title"><a href="rewardedcriminaldisplay.php">Rewarded Criminal</a></h5>
+
+                <div class="box-icon-modern-decor"></div>
+
+                <a href="rewardedcriminaldisplay.php" class="knowmorebtn-services">View More</a>
+              </article>
+
+            </div>
+
+            <div class="col-md-6 col-lg-4">
+
+              <article class="box-icon-modern">
+
+                <img src="images/lookout.png" alt="" width="270" height="300"/>
+
+                <h5 class="box-icon-modern-title"><a href="noflydisplay.php">Lookout Notice</a></h5>
+
+                <div class="box-icon-modern-decor"></div>
+
+                <a href="noflydisplay.php" class="knowmorebtn-services">View More</a>
+              </article>
+
+            </div>
+
+
+          </div>
+
+        </div>
+
+      </section>
+
+  <?php include('footer.php');?>
